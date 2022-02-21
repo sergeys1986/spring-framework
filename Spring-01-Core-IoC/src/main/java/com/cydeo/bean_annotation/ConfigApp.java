c2960ab4c0e6fdbe8c12d446b957f026369efb47
sergeys1986 <sergeypakhotin@gmail.com>
2022-02-21 07:34:02 -0500

    New classes added

---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/ConfigApp.java
@@ -0,0 +1,9 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigApp {
+
+
+}
---
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
@@ -0,0 +1,12 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext();
+
+    }
+}
