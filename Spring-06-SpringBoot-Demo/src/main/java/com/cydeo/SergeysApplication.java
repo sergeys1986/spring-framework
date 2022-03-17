17cd5dc94ac063e91b8e93f2bfc70011ea6836d0
sergeys1986 <sergeypakhotin@gmail.com>
2022-03-17 19:50:31 -0400

    Thymeleaf

---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/SergeysApplication.java
@@ -0,0 +1,14 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SergeysApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SergeysApplication.class, args);
+    }
+
+}
---
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/Spring07SpringMvcControllerApplication.java
@@ -0,0 +1,24 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+Spring07SpringMvcControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring07SpringMvcControllerApplication.class, args);
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
